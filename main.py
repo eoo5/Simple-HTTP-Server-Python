50861b985e3042a48d3c8ad6deef2363e6d01697
Erasto Omolo <eoo5@pitt.edu>
2024-06-15 20:12:01 -0400

    finished

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import time
 
 HOST = "localhost"  # or "" to bind to all interfaces
 PORT = 8000  # replace with the port you want to use
@@ -10,6 +11,14 @@ class NeuralHTTP(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes("<html><body><h1> HELLO WORLD!</h1></body></html>", "utf-8"))
+
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+        date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+        self.wfile.write(bytes(f'{{"time":"{date}"}}', "utf-8"))
 
 server = HTTPServer((HOST, PORT), NeuralHTTP)
 print(f"Starting server on {HOST}:{PORT}")
